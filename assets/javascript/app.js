d2a4d4ba58a42379719bef08d93d1ffd6f706e0d
Carlos Gasperi <cgasperi90@gmail.com>
2020-03-09 12:32:34 -0400

    had not saved the JS file. Adding variables now to the JS file

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+//we need to start by selecting all the html elements within the quiz div
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+
+//here we will select the question divs
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+//here we will select the score or the results div
+var scoreContainer = document.getElementById("scoreContainer");
