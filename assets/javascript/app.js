e7f21a367d11e3539b422dd0aeecff091afb11be
Carlos Gasperi <cgasperi90@gmail.com>
2020-03-09 23:04:09 -0400

    pushing code for javascript file, css file, and html file. This completes and fixes all errors on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,167 @@
+//there is the variables for the entire page that grab the html elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
 
-//we need to start by selecting all the html elements within the quiz div
-var start = document.getElementById("start");
-var quiz = document.getElementById("quiz");
-var question = document.getElementById("question");
-var counter = document.getElementById("counter");
-var timeGauge = document.getElementById("timeGauge");
+//This is the array that holds the questions and the answers along with the correct answer
+let questions = [
+    {
+        question : "How many teams are there in the NFL?",
+        imgSrc : "img/html.png",
+        choiceA : "32",
+        choiceB : "28",
+        choiceC : "30",
+        correct : "A"
+    },{
+        question : "How many weeks are in an NFL Season?",
+        imgSrc : "img/css.png",
+        choiceA : "18",
+        choiceB : "17",
+        choiceC : "19",
+        correct : "B"
+    },{
+        question : "How many players are an NFL team?",
+        imgSrc : "img/js.png",
+        choiceA : "50",
+        choiceB : "48",
+        choiceC : "53",
+        correct : "C"
+    },
+    {
+        question: "How many teams are in a division?",
+        choiceA: "3",
+        choiceB: "5",
+        choiceC: "4",
+        correct: "C"
+    },
+    {
+        question: "What is the trophy given in the Super Bowl called?",
+        choiceA: "Super Bowl Trophy",
+        choiceB: "Vince Lombardi Trophy",
+        choiceC: "George Halas Trophy",
+        correct: "B"
+    }
+];
 
-//here we will select the question divs
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-//here we will select the score or the results div
-var scoreContainer = document.getElementById("scoreContainer");
+//These are the variables for the entire page
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
+
+// This function puts the question on the html page
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click",startQuiz);
+
+// This function starts the quiz when the user clicks the button to start.
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+}
+
+// This function places the progress bar on the page
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// This function places the counter timer on the page
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red when the user doesnt answer the question
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// This function checks the answer when the user clicks an answer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green when the user answers correctly
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+// answer is correct
+function answerIsCorrect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+// answer is Wrong
+function answerIsWrong(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// This function places the score on the html page when the user finishes the trivia game.
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+}
